62976137b177852c52033ea3d273fac237461407
Alexey Trofimov <alexzonic@yandex.ru>
2022-02-12 20:46:13 +0400

    подключил sass

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import "./Button.css";
+import "./Button.scss";
 
 interface Props {
     value: string;
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import './Input.css'
+import './Input.scss'
 
 interface Props {
     value: string;
